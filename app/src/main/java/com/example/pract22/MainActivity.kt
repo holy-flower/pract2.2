63433cad5ff37883c92c6dbfe4c739fd339e239b
holy-flower <prodolnova04@mail.ru>
2024-09-23 20:36:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pract22/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.pract22
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.pract22.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+
+        binding.buttonFirst.setOnClickListener {
+            replaceFragment(FirstFragment())
+        }
+
+        binding.buttonSecond.setOnClickListener {
+            replaceFragment(SecondFragment())
+        }
+
+        binding.buttonThird.setOnClickListener {
+            replaceFragment(ThirdFragment())
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
